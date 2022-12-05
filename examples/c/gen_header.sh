4b562ebd5f7e258d916b218f7b563dc1c552c251
thomas <thomas.chiantia@gmail.com>
2022-12-05 15:27:28 -0500

    ...

--- examples/c/gen_header.sh
+++ examples/c/gen_header.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 
-# NOTE we call cbindgen outside of build.rs because cbindgen expand seems to suck when called from a build.rs file
-cbindgen --config cbindgen.toml --lang c . | clang-format > target/binding.h
+# NOTE we call cbindgen outside of build.rs because cbindgen expand seems to 
+#      suck when called from a build.rs file
+cbindgen --config cbindgen.toml --lang c --clean . | clang-format > target/binding.h
--- examples/c/src/lib.rs
+++ examples/c/src/lib.rs
@@ -6,4 +6,6 @@
 #![allow(unused)]
 #![cfg_attr(not(test), no_std)]
 
+pub use minicbor_sys::*;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
