447e8d7e81efd11189b60a28639c9b7f18da126f
thomas <thomas.chiantia@gmail.com>
2022-12-05 15:11:12 -0500

    hang workaround

--- examples/c/build.rs
+++ examples/c/build.rs
@@ -13,15 +13,5 @@ fn main() -> result::Result<(), minicbor_bindgen::Error> {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
     fs::write(out, lib).expect("failed to write bindings");
 
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .with_parse_include(&["c", "minicbor-sys"])
-        // .with_parse_extra_bindings(&["minicbor-sys"])
-        .with_parse_expand(&["c", "minicbor-sys"])
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file("bindings.h");
     Ok(())
 }
---
+++ examples/c/gen_header.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+# NOTE we call cbindgen outside of build.rs because cbindgen expand seems to suck when called from a build.rs file
+cbindgen --config cbindgen.toml --lang c . | clang-format > binding.h
